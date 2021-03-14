b47bbc47e49f81147b527c641fbc65ecd4926b4a
Vladimir Bredikhin <vladimir.bredikhin.dev@gmail.com>
2021-03-15 00:49:40 +0200

    Our App Data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Player from './components/Player';
 import Song from './components/Song';
 import './styles/app.scss';
+import chillhop from './data';
 
 const App = () => {
+  const data = chillhop();
+  console.log(data);
+
   return (
     <div>
       <Song />
