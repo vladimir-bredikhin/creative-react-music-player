2f23a8b6583a7c1c90563fad1d50b5100e39d21f
Vladimir Bredikhin <vladimir.bredikhin.dev@gmail.com>
2021-03-15 13:05:56 +0200

    Use Ref

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div>
       <Song currentSong={currentSong} />
-      <Player />
+      <Player currentSong={currentSong} />
     </div>
   );
 };
--- src/components/Player.js
+++ src/components/Player.js
@@ -4,25 +4,37 @@ import {
   faPlay,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRef } from 'react';
 
-const Player = () => {
+const Player = ({ currentSong }) => {
+  const audioRef = useRef(null);
+  const playSongHandler = () => {
+    audioRef.current.play();
+  };
+
   return (
     <>
       <div className='player'>
         <div className='time-control'>
           <p>Start time</p>
-          <input type='range'  />
+          <input type='range' />
           <p>End Time</p>
         </div>
         <div className='play-control'>
           <FontAwesomeIcon className='skip-back' icon={faAngleLeft} size='2x' />
-          <FontAwesomeIcon className='play' icon={faPlay} size='2x' />
+          <FontAwesomeIcon
+            onClick={playSongHandler}
+            className='play'
+            icon={faPlay}
+            size='2x'
+          />
           <FontAwesomeIcon
             className='skip-forward'
             icon={faAngleRight}
             size='2x'
           />
         </div>
+        <audio ref={audioRef} src={currentSong.audio}></audio>
       </div>
     </>
   );
